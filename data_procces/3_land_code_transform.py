53f783633b893cdc5ad6fa30c4c94cc99a4ea14d
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-07-29 17:50:32 +0800

    Update 3_land_code_transform.py

--- data_procces/3_land_code_transform.py
+++ data_procces/3_land_code_transform.py
@@ -11,56 +11,74 @@ import os
 import json
 import time
 import argparse
-import requests
 import pandas as pd
 
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from IPython import embed
 
 def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--file_path', type=str, default='data/procces/2_coordinate_data/crawler_miss.csv')
     parser.add_argument('--output_folder', type=str, default='data/procces/3_land_code_transform')
-    parser.add_argument('--chrome_driver', type=str, default='tool/chromedriver.exe')
 
     args = parser.parse_args()
 
     return args
 
-def get_new_land_code(item, chrome_driver):
+def get_new_land_code(df):
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--incognito")
+    # chrome_options.add_argument("--headless") # 無視窗
+    chrome_options.add_argument("--incognito") # 無痕
+    chrome_options.add_argument("--disable-software-rasterizer") # 無痕
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging']) # 忽略 log
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36")
 
-    try:
-        browser = webdriver.Chrome(chrome_driver,
-                                    chrome_options=chrome_options)
+    land_addr_dt_ls = df.to_dict(orient="records")
 
-        browser.implicitly_wait(10) # 隱式等待
+    for i in land_addr_dt_ls:
+        try:
+            embed()
+            exit()
+            browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+            
+            browser.implicitly_wait(10) # 隱式等待
 
-        browser.get("https://www.land.tycg.gov.tw/chaspx/SQry4.aspx/14")
+            browser.get("https://www.land.tycg.gov.tw/chaspx/SQry4.aspx/14")
+            count = 1                  
+            while count < 3:
+                # 進入到互動 iframe
+                browser.switch_to.frame(browser.find_element(By.XPATH, '//*[@title="新舊地建號查詢"]'))
+
+                # 選擇搜尋服務類型
+                type_select = Select(browser.find_element('name', 'searchtype'))
+                type_select.select_by_visible_text('舊地號查新地號')
+                # select.select_by_value('2')
+                
+                city_select = Select(browser.find_element('name', 'xcity'))
+                city_select.select_by_visible_text('桃園區') #桃園區(01)
+                
+                time.sleep(0.5)
+                htmlstring = browser.page_source
+                soup_map = BeautifulSoup(htmlstring, 'lxml')
+
+                count+=1
+
+            soup_map_clear = soup_map.find(class_="section-popular-times-container")
+
+        except:
+            print('error')
         
-        count = 1                  
-        while count < 3:
-                
-            time.sleep(3+count)
-            htmlstring = browser.page_source
-            soup_map = BeautifulSoup(htmlstring, 'lxml')
-
-            if soup_map.find(class_="section-popular-times-container") != None:
-                browser.close()
-                break
-                
-            count+=1
-
-        soup_map_clear = soup_map.find(class_="section-popular-times-container")
-
-    except:
-        print('error')
+        browser.close()
 
 def saveJson(data, path, mode):
     if mode == 'nor_write':
@@ -84,13 +102,10 @@ def main():
 
     # 讀取資料
     assert os.path.exists(args.file_path), f"Input file could not be found at {args.file_path}"
-    df = pd.read_csv(args.file_path, low_memory=False)
-
-    assert args.landid_col in df.columns, f"{args.landid_col} attribute could not be found in input file"
-    land_addr_df = df[args.landid_col].drop_duplicates().reset_index(drop=True)
+    df = pd.read_csv(args.file_path, low_memory=False).drop_duplicates()
 
     # 爬取座標資料
-    coordinate_df = get_new_land_code(df, args.chrome_driver)
+    coordinate_df = get_new_land_code(df)
 
 if __name__ == '__main__':
     main()
