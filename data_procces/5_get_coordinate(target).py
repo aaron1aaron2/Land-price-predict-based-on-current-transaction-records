596a2c78405f54d5940fa1e45215d672e91d2c13
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-02 14:56:14 +0800

    Update 5_get_coordinate(target).py

--- data_procces/5_get_coordinate(target).py
+++ data_procces/5_get_coordinate(target).py
@@ -4,7 +4,7 @@ Github: https://github.com/aaron1aaron2
 Email: aaron1aaron2@gmail.com
 Create Date: 2022.08.02
 Last Update: 2022.08.02
-Describe: 獲取土地經緯度座標，來源 「地號 GeoJSON API」。2015 年的資料 -> https://twland.ronny.tw/
+Describe: 爬蟲部分大致上和 2_get_coordinate.py 功能依樣，這部分是爬取上步驟轉完的新地號
 """
 
 import os
@@ -22,40 +22,30 @@ def get_args():
     parser.add_argument('--file_path', type=str, default='data/procces/1_sort_data/transaction_land.csv')
     parser.add_argument('--output_folder', type=str, default='data/procces/2_coordinate_data')
 
-    parser.add_argument('--landid_col', type=str, default='土地位置', help='土地位置與地號欄位名稱')
-
-    parser.add_argument('--extract_pattern', type=str, 
-                        default='(?P<county>\w+市)(?P<district>\w+區)(?P<section>\w+段)(?P<number>.+)地號', 
-                        help='土地資訊切分 regex pattern。須包含 county、district、section、number 四個資訊，可參考 default format。')
+    parser.add_argument('--county_col', type=str, default='county')
+    parser.add_argument('--district_col', type=str, default='district')
+    parser.add_argument('--section_col', type=str, default='section_new')
+    parser.add_argument('--number_col', type=str, default='number_new')
 
     args = parser.parse_args()
 
     return args
 
-def extract_pattern(df, pattern, output):
-    extract_df = df.str.extract(pattern)
-
-    # 輸出檢查用資料
-    df[extract_df.isna().any(axis=1)].to_csv(os.path.join(output, 'extract_miss.csv'))
-    pd.DataFrame(df).join(extract_df).to_csv(os.path.join(output, 'extract_result.csv'), index=False)
-
-    return extract_df
-
-def get_coordinate(df, output):
+def get_coordinate(df:pd.DataFrame, output:str, county:str, district:str, section:str, number:str) -> None:
     result_path = os.path.join(output, 'crawler_result.csv')
     miss_path = os.path.join(output, 'crawler_miss.csv')
 
     print(f'Total tasks: {df.shape[0]}')
     # 爬蟲進度恢復
     if os.path.exists(result_path):
-        tmp = df['county'] + df['district'] + df['section'] + df['number']
+        tmp = df[county] + df[district] + df[section] + df[number]
 
         progress_df = pd.read_csv(result_path)
         miss_df = pd.read_csv(miss_path)
 
         all_df = pd.concat([progress_df, miss_df])
 
-        df = df[~tmp.isin(all_df['county'] + all_df['district'] + all_df['section'] + all_df['number'])]
+        df = df[~tmp.isin(all_df[county] + all_df[district] + all_df[section] + all_df[number])]
 
         del tmp; del progress_df; del miss_df
 
@@ -64,7 +54,7 @@ def get_coordinate(df, output):
     # 爬蟲
     land_addr_dt_ls = df.to_dict(orient="records")
     for i in tqdm(land_addr_dt_ls):
-        url = f"https://twland.ronny.tw/index/search?lands[]={i['county']},{i['section']},{i['number']}"
+        url = f"https://twland.ronny.tw/index/search?lands[]={i[county]},{i[section]},{i[number]}"
         r = requests.get(url)
 
         if r.status_code != 200:
@@ -93,10 +83,10 @@ def get_coordinate(df, output):
 
         time.sleep(0.5)
 
-def output_csv(data, path):
+def output_csv(data:dict, path:str) -> None:
     pd.DataFrame([data]).to_csv(path, mode='a', index=False, header=not os.path.exists(path))
     
-def saveJson(data, path, mode):
+def saveJson(data:dict, path:str, mode:str) -> None:
     if mode == 'nor_write':
         with open(path, 'w', encoding='utf-8') as outfile:  
             json.dump(data, outfile, indent=2, ensure_ascii=False)
@@ -120,14 +110,8 @@ def main():
     assert os.path.exists(args.file_path), f"Input file could not be found at {args.file_path}"
     df = pd.read_csv(args.file_path, low_memory=False)
 
-    assert args.landid_col in df.columns, f"{args.landid_col} attribute could not be found in input file"
-    land_addr_df = df[args.landid_col].drop_duplicates().reset_index(drop=True)
-
-    # 抽取土地資訊
-    extract_df = extract_pattern(land_addr_df, args.extract_pattern, args.output_folder)
-
     # 爬取座標資料
-    coordinate_df = get_coordinate(extract_df, args.output_folder)
+    get_coordinate(df, args.output_folder, args.county_col, args.district_col, args.section_col, args.number_col)
 
 if __name__ == '__main__':
     main()
