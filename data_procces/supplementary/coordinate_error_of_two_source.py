7131d8a87cfef08537d1e8d6f73f41229819a0e7
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-03 16:14:54 +0800

    Create coordinate_error_of_two_source.py

---
+++ data_procces/supplementary/coordinate_error_of_two_source.py
@@ -0,0 +1,9 @@
+"""
+Author: 何彥南 (yen-nan ho)
+Github: https://github.com/aaron1aaron2
+Email: aaron1aaron2@gmail.com
+Create Date: 2022.08.03
+Last Update: 2022.08.03
+Describe: 經手動與 google map 交叉確認「地籍圖資網路便民服務系統」的誤差較小，但是爬取速度太慢且會擋 ip，爬取全部土地位置估計需要 10 天。
+            這邊希望比較 「地籍圖資網路便民服務系統」 與 「地號 GeoJSON API」 兩種地號轉經緯服務差距。可以調整到「地號 GeoJSON API」
+"""
