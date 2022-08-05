658e161e56a031f3fd3bc4c73ab5207209feb755
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-05 16:10:55 +0800

    Create 7.1_miss_land_distribute.py

---
+++ data_procces/supplementary/7.1_miss_land_distribute.py
@@ -0,0 +1,8 @@
+"""
+Author: 何彥南 (yen-nan ho)
+Github: https://github.com/aaron1aaron2
+Email: aaron1aaron2@gmail.com
+Create Date: 2022.08.05
+Last Update: 2022.08.05
+Describe: 觀察位爬未爬取到的土地位置 1萬5千多筆與有爬取到 5萬3千多筆土地。在交易時間上的分布
+"""
