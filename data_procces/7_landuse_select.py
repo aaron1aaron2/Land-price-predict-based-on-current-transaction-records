27586cdb88996427773b1aeb568cfddc63cabf60
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-05 17:59:00 +0800

    Update 7_landuse_select.py

--- data_procces/7_landuse_select.py
+++ data_procces/7_landuse_select.py
@@ -1,3 +1,11 @@
+"""
+Author: 何彥南 (yen-nan ho)
+Github: https://github.com/aaron1aaron2
+Email: aaron1aaron2@gmail.com
+Create Date: 2022.08.05
+Last Update: 2022.08.05
+Describe: 土地使用分區的分類與篩選。
+"""
 import pandas as pd
 
 df = pd.read_csv('data/procces/6_sort_crawler_data/transaction_coordinate_use.csv')
