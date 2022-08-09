7d334eea91c4aef068456b1059ba3ace004d4aca
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-09 15:49:33 +0800

    Delete 8_target_land_group.py

--- data_procces/8_target_land_group.py
+++
@@ -1,8 +0,0 @@
-"""
-Author: 何彥南 (yen-nan ho)
-Github: https://github.com/aaron1aaron2
-Email: aaron1aaron2@gmail.com
-Create Date: 2022.08.05
-Last Update: 2022.08.05
-Describe: 許多目標土地位置上很相近，所以在這步驟會使用 DBSCAN 聚類方式對土地點分群。讓位置相近的土地歸到成同一個目標點。
-"""
