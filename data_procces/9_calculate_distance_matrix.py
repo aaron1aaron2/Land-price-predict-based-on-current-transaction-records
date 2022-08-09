e376ffdd6ffd5a717f77d0bc4e6a12fbce65f9a0
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-09 15:49:27 +0800

    Delete 9_calculate_distance_matrix.py

--- data_procces/9_calculate_distance_matrix.py
+++
@@ -1,8 +0,0 @@
-"""
-Author: 何彥南 (yen-nan ho)
-Github: https://github.com/aaron1aaron2
-Email: aaron1aaron2@gmail.com
-Create Date: 2022.08.05
-Last Update: 2022.08.05
-Describe: 獲取所有時價登入土地與目標土地經緯度之間的直線距離。
-"""
