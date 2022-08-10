3e080ab18197056d0e3d14a7e602e255f191631a
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-10 15:29:18 +0800

    Update 7_landuse_select.py

--- data_procces/7_landuse_select.py
+++ data_procces/7_landuse_select.py
@@ -10,7 +10,6 @@ import os
 import pandas as pd
 
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # 基本設定
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
@@ -29,17 +28,25 @@ plt.savefig(os.path.join(output_folder, 'target_landuse_pie.png'))
 df = pd.read_csv('data/procces/6_sort_crawler_data/transaction_coordinate_use.csv', dtype=str)
 
 landuse_df = df[['都市土地使用分區', '非都市土地使用分區', '非都市土地使用編定']].drop_duplicates().reset_index(drop=True)
-landuse_df.fillna('無', inplace=True)
 
-tmp = landuse_df['都市土地使用分區'] + '|' + landuse_df['非都市土地使用分區'] + '|' + landuse_df['非都市土地使用編定']
+tmp = landuse_df['都市土地使用分區'].fillna('無') + '|' + landuse_df['非都市土地使用分區'].fillna('無') + '|' +\
+        landuse_df['非都市土地使用編定'].fillna('無')
 
-landuse_df.loc[tmp.str.find('農') != -1, '類型'] = '農'
-landuse_df.loc[tmp.str.find('住') != -1, '類型'] = '住'
-landuse_df.loc[tmp.str.find('商') != -1, '類型'] = '商'
-landuse_df.loc[tmp.str.find('工') != -1, '類型'] = '工'
+landuse_df.loc[tmp.str.find('農') != -1, '使用分區'] = '農'
+landuse_df.loc[tmp.str.find('住') != -1, '使用分區'] = '住'
+landuse_df.loc[tmp.str.find('商') != -1, '使用分區'] = '商'
+landuse_df.loc[tmp.str.find('工') != -1, '使用分區'] = '工'
 
-landuse_df['類型'] = landuse_df['類型'].fillna('其他')
+landuse_df['使用分區'] = landuse_df['使用分區'].fillna('其他')
 
-landuse_df.to_csv('')
+landuse_df.to_csv(os.path.join(output_folder, 'landuse_sort.csv'), index=False)
 
+df = df.merge(landuse_df, on= ['都市土地使用分區', '非都市土地使用分區', '非都市土地使用編定'], how='left')
 
+df.to_csv(os.path.join(output_folder, 'transaction_all.csv'), index=False)
+
+# 觀察分布
+plt.clf()
+df['使用分區'].value_counts().plot(kind='pie', rot=0, cmap=plt.get_cmap('Pastel1'), 
+                autopct='%1.0f%%', pctdistance=0.8, labeldistance=1.1)
+plt.savefig(os.path.join(output_folder, 'target_landuse_pie.png'))
