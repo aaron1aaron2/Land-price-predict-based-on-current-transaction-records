46d680ee578af4aa1e4c4aea96f505300a050c5d
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-12 18:25:05 +0800

    Update 8_time_range_select.py

--- data_procces/8_time_range_select.py
+++ data_procces/8_time_range_select.py
@@ -27,6 +27,8 @@ df['year_TW'] = df['year']
 df['year'] = df['year'].astype(int) + 1911
 df['date'] = pd.to_datetime(df[["year", "month", "day"]].astype(int))
 
+df = df[df['year_TW']!='100'] # 不應該存在 100 年的資料
+
 # 每月份的交易量
 tmp = df[["year", "month", "day"]].astype(int)
 tmp['day'] = 1
@@ -41,9 +43,9 @@ plt.clf()
 # plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%b')) # define the formatting
 # plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=12)) # show every 12th tick on x axes
 # plt.xticks(rotation=40, fontweight='light', fontsize='x-small')
-sns.set(rc={'figure.figsize':(13, 8)})
 sns.lineplot(x='year-month', y='count', hue='year', data=month_ct.reset_index(), palette='Set2')
 
+plt.tight_layout()
 plt.savefig(os.path.join(output_folder, 'transaction_plot(year-month).png'))
 
 # 每個月在不同 ================================
@@ -58,10 +60,16 @@ gb_ct.rename(columns={'day': 'count'}, inplace=True)
 plt.clf()
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
 sns.lineplot(x='year-month', y='count', hue='使用分區', data=gb_ct, palette='Set2')
+plt.tight_layout()
 plt.savefig(os.path.join(output_folder, 'transaction_landuse_plot(year-month).png'))
 
 
 plt.clf()
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
 sns.lineplot(x='year-month', y='count', hue='使用分區', data=gb_ct[gb_ct['使用分區'].isin(['住', '商'])], palette='Set2')
+plt.tight_layout()
 plt.savefig(os.path.join(output_folder, 'transaction_landuse_plot(year-month)_house-business.png'))
+
+# 資料篩選 ===================================\
+
+df
