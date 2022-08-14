f4e2a19fe54deb069506d0699f215d69c737a121
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-14 11:17:59 +0800

    Update 9_m1_target_land_group.py

--- data_procces/9_m1_target_land_group.py
+++ data_procces/9_m1_target_land_group.py
@@ -8,15 +8,16 @@ Last Update: 2022.08.05
 Describe: 許多目標土地位置上很相近，所以在這步驟會使用 DBSCAN 聚類方式對土地點分群。讓位置相近的土地歸到成同一個目標點。
 """
 import os
+from unittest import result
 import folium
 import itertools
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+
+from matplotlib import cm, colors
 
 from sklearn.cluster import DBSCAN
-from sklearn import metrics
 from geopy.distance import geodesic
+
 
 output_folder = 'data/procces/9_m1_target_land_group'
 os.makedirs(output_folder, exist_ok=True)
@@ -24,7 +25,6 @@ os.makedirs(output_folder, exist_ok=True)
 df = pd.read_csv('data/procces/5_get_coordinate(target)/crawler_result_mod.csv')
 
 df['id'] = df.index
-df.to_csv(os.path.join(output_folder, 'target_land.csv'), index=False)
 
 # 建立目標土地的 distanc matrix ========================================
 df = pd.read_csv(os.path.join(output_folder, 'target_land.csv'), usecols=['id', 'lat', 'long'])
@@ -60,46 +60,41 @@ df_all = pd.concat([df_AB, df_BA])
 distanct_mat = df_all.pivot_table(values='linear_distance', index=['start_id'], columns=['end_id'])
 distanct_mat.fillna(0, inplace=True)
 
-# DBSCAN 分群 =========================================================
+# DBSCAN 分群 & 畫地圖 =========================================================
+colors_ls = cm.get_cmap('Set1') # 0-7
 
-db = DBSCAN(eps=0.005, min_samples=10, metric='precomputed').fit(data)
-labels = db.labels_
-raito = len(labels[labels[:] == -1]) / len(labels) # 計算噪聲點個數佔總數的比例
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0) # 獲取分簇的數目
+for i in [100, 200, 300, 400, 500]:
+    db = DBSCAN(eps=i, min_samples=2, metric='precomputed').fit(distanct_mat) # precomputed 代表使用距離矩陣
+    df[f'DBSCAN_{i}'] = db.labels_
 
-score = metrics.silhouette_score(data, labels)
- 
-df['label'] = labels
-sns.lmplot('lat_Amap', 'lng_Amap', df, hue='label', fit_reg=False)
+    map_ = folium.Map(
+                [24.961640, 121.142500],
+                zoom_start=12 )
 
-## 第二部分
-map_ = folium.Map(
-            [24.961640, 121.142500],
-            zoom_start=12 )
+    for idx, coor, group  in df[['id', 'coordinate', f'DBSCAN_{i}']].values:
+        color = colors.to_hex(colors_ls(group%8)) if group!=-1 else 'black'
+        folium.CircleMarker(location=coor.split(','),
+                            radius=4, popup='popup',
+                            color=color, 
+                            fill=True,
+                            fill_color=color).add_to(map_)
+        
+    map_.save(os.path.join(output_folder, f'DBSCAN_{i}.html'))
 
-colors = ['#DC143C', '#FFB6C1', '#DB7093', '#C71585', '#8B008B', '#4B0082', '#7B68EE',
-         '#0000FF', '#B0C4DE', '#708090', '#00BFFF', '#5F9EA0', '#00FFFF', '#7FFFAA',
-         '#008000', '#FFFF00', '#808000', '#FFD700', '#FFA500', '#FF6347','#DC143C', 
-         '#FFB6C1', '#DB7093', '#C71585', '#8B008B', '#4B0082', '#7B68EE',
-         '#0000FF', '#B0C4DE', '#708090', '#00BFFF', '#5F9EA0', '#00FFFF', '#7FFFAA',
-         '#008000', '#FFFF00', '#808000', '#FFD700', '#FFA500', '#FF6347', 
-         '#DC143C', '#FFB6C1', '#DB7093', '#C71585', '#8B008B', '#4B0082', '#7B68EE',
-         '#0000FF', '#B0C4DE', '#708090', '#00BFFF', '#5F9EA0', '#00FFFF', '#7FFFAA',
-         '#008000', '#FFFF00', '#808000', '#FFD700', '#FFA500', '#FF6347',
-         '#DC143C', '#FFB6C1', '#DB7093', '#C71585', '#8B008B', '#4B0082', '#7B68EE',
-         '#0000FF', '#B0C4DE', '#708090', '#00BFFF', '#5F9EA0', '#00FFFF', '#7FFFAA',
-         '#008000', '#FFFF00', '#808000', '#FFD700', '#FFA500', '#FF6347','#DC143C', 
-         '#FFB6C1', '#DB7093', '#C71585', '#8B008B', '#4B0082', '#7B68EE',
-         '#0000FF', '#B0C4DE', '#708090', '#00BFFF', '#5F9EA0', '#00FFFF', '#7FFFAA',
-         '#008000', '#FFFF00', '#808000', '#FFD700', '#FFA500', '#FF6347', 
-         '#DC143C', '#FFB6C1', '#DB7093', '#C71585', '#8B008B', '#4B0082', '#7B68EE',
-         '#0000FF', '#B0C4DE', '#708090', '#00BFFF', '#5F9EA0', '#00FFFF', '#7FFFAA',
-         '#008000', '#FFFF00', '#808000', '#FFD700', '#FFA500', '#FF6347','#000000']
+df.to_csv(os.path.join(output_folder, 'DBSCAN_result.csv'), index=False)
 
-for i in range(len(data)):
-    folium.CircleMarker(location=[data[i][0], data[i][1]],
-                        radius=4, popup='popup',
-                        color=colors[labels[i]], fill=True,
-                        fill_color=colors[labels[i]]).add_to(map_)
-    
-map_.save('all_cluster.html')
+
+cur_id = df['DBSCAN_500'].max() +1
+
+result = []
+for i in df['DBSCAN_500'].to_list():
+    result.append(i) if i !=-1 else result.append(cur_id)
+    if i==-1: cur_id += 1
+
+df['group_id'] = result
+
+df_org = pd.read_csv(os.path.join(output_folder, 'target_land.csv'))
+df = df_org.merge(df[['id', 'coordinate', 'group_id']], how='left')
+
+df.drop('result_text', axis=1, inplace=True)
+df.to_csv(os.path.join(output_folder, 'target_land_group.csv'), index=False)
