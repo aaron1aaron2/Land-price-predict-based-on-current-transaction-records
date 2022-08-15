023ff4e57bfb3906d5c54b082a2cb5aba17995de
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-15 18:17:30 +0800

    Create data_helper_method.py

---
+++ scripts/data_helper_method.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+"""
+Author: 何彥南 (yen-nan ho)
+Github: https://github.com/aaron1aaron2
+Email: aaron1aaron2@gmail.com
+Create Date: 2022.08.15
+Last Update: 2022.08.15
+Describe: 集合所有方法步驟，可以一次性的透過參數設定整理資料。
+"""
