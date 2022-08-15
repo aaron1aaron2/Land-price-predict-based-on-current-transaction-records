c5c060fe7ec788e6fc0fc3370053c985b3c30f1e
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-15 18:17:35 +0800

    Update 1_reference_point.py

--- code/method_procces/1_reference_point.py
+++ code/method_procces/1_reference_point.py
@@ -4,14 +4,15 @@ Github: https://github.com/aaron1aaron2
 Email: aaron1aaron2@gmail.com
 Create Date: 2022.08.14
 Last Update: 2022.08.14
-Describe: 設立對應的參考點
+Describe: 設立對應的參考點。上下左右
 """
 import os
 import pandas as pd
 
-from sklearn.cluster import DBSCAN
 from geopy.distance import geodesic
 
 
 output_folder = 'data/method_procces/1_reference_point'
 os.makedirs(output_folder, exist_ok=True)
+
+df = pd.read_csv('data/method_procces/0_target_land_group/target_land_group.csv')
