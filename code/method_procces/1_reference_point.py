631d604c2e7f9582fc63e223be5e47a72b2f6197
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-18 15:31:08 +0800

    Update 1_reference_point.py

--- code/method_procces/1_reference_point.py
+++ code/method_procces/1_reference_point.py
@@ -4,7 +4,7 @@ Github: https://github.com/aaron1aaron2
 Email: aaron1aaron2@gmail.com
 Create Date: 2022.08.14
 Last Update: 2022.08.14
-Describe: 設立對應的參考點。上下左右
+Describe: 設立對應的參考點。上下左右(3000公尺)的位置。關於範圍設定可以看 code/supplementary/m0__effective_range_select.py
 """
 import os
 import pandas as pd
