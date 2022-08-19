0412b81d72f7d4f88293351835fdb2e5dee29d0a
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-19 18:22:26 +0800

    Update 1_reference_point.py

--- code/method_procces/1_reference_point.py
+++ code/method_procces/1_reference_point.py
@@ -8,17 +8,18 @@ Describe: 設立對應的參考點。上下左右(3000公尺)的位置。
             1. 經緯度加減度數對應距離，由 google map 桃園市中心點為基準測試 -> (24.9605666,121.2188135)
             2. 關於範圍設定可以看 code/supplementary/m0__effective_range_select.py
 """
-from math import degrees
 import os
+
 import decimal
 import pandas as pd
 
+from functools import partial
 from geopy.distance import geodesic
 
 output_folder = 'data/method_procces/1_reference_point'
 os.makedirs(output_folder, exist_ok=True)
 
-df = pd.read_csv('data/method_procces/0_target_land_group/target_land_group.csv')
+df = pd.read_csv('data/method_procces/0_target_land_group/group_list.csv')
 
 # 經緯度轉換距離測試 ---------------------------------------
 decimal_places = lambda val: abs(decimal.Decimal(str(val)).as_tuple().exponent)
@@ -53,7 +54,7 @@ down_degree = near_point.loc[0, 'degree']
 assert (up_distance >= 100) & (down_distance <= 100), 'The range does not contain 100 meters'
 
 lat_degree = ((100-down_distance)/(up_distance-down_distance)*(up_degree-down_degree)) + down_degree
-# degree = 0.0009027527123486883
+# lat_degree = 0.0009027527123486883
 
 # 經度
 test_interval = 0.0005
@@ -81,7 +82,32 @@ down_degree = near_point.loc[0, 'degree']
 assert (up_distance >= 100) & (down_distance <= 100), 'The range does not contain 100 meters'
 
 long_degree = ((100-down_distance)/(up_distance-down_distance)*(up_degree-down_degree)) + down_degree
+# long_degree = 0.0009902726875066093
+
+# 產生各群的參考點 ---------------------------------------
+def get_reference(coordinate, target_meter, long_per_100_meter, lat_per_100_meter):
+    lat, long = coordinate.split(',')
+
+    lat_degree = target_meter*lat_per_100_meter/100
+    long_degree = target_meter*long_per_100_meter/100
+
+    result = {
+        'group_center': coordinate,
+        'refer_point1': f'{float(lat)+lat_degree},{long}',
+        'refer_point2': f'{float(lat)-lat_degree},{long}',
+        'refer_point3': f'{lat},{float(long)+long_degree}',
+        'refer_point4': f'{lat},{float(long)-long_degree}'
+    }
 
 
-def get_reference(meter):
-    pass
+    return result
+
+result_ls = list(map(partial(
+        get_reference, 
+        target_meter=3000, 
+        long_per_100_meter=long_degree, 
+        lat_per_100_meter=lat_degree
+    ), df['group_center'].to_list()))
+
+df = df.merge(pd.DataFrame(result_ls), how='left')
+df.to_csv(os.path.join(output_folder, 'reference_point.csv'), index=False)
