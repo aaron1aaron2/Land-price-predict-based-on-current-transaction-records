e5243ad498f42432addd2e32c043e832e4a78fff
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-19 19:01:16 +0800

    Update 2_calculate_distance_matrix.py

--- code/method_procces/2_calculate_distance_matrix.py
+++ code/method_procces/2_calculate_distance_matrix.py
@@ -7,18 +7,19 @@ Create Date: 2022.08.14
 Last Update: 2022.08.14
 Describe: 獲取所有時價登入土地與目標土地經緯度之間的直線距離。
 """
-
 import os
 import itertools
 import pandas as pd
 
 from geopy.distance import geodesic
 
-output_folder = 'data/supplementary'
+output_folder = 'data/method_procces/2_calculate_distance_matrix'
 os.makedirs(output_folder, exist_ok=True)
 
-df_tar = pd.read_csv('data/method_procces/0_target_land_group/group_list.csv')
-df_tran = pd.read_csv('data/data_procces/8_time_range_select/transaction_all.csv')
+df_tar = pd.read_csv('data/method_procces/1_reference_point/reference_point.csv')
+df_tran = pd.read_csv('data/data_procces/8_time_range_select/transaction_all.csv', usecols=['long', 'lat', 'land_id'])
+df_tran.drop_duplicates(inplace=True)
+df_tran['land_id'] = df_tran['land_id'].astype(int)
 
 tran_land = df_tran[['long', 'lat', 'land_id']].drop_duplicates()
 tran_land['tran_land_center'] = tran_land['lat'].astype(str) + ',' +  tran_land['long'].astype(str)
