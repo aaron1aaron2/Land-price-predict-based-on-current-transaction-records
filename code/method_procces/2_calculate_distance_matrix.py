c5b4d8a9149fb22f429e5b5e4001f67123b53625
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-23 15:51:09 +0800

    Update 2_calculate_distance_matrix.py

--- code/method_procces/2_calculate_distance_matrix.py
+++ code/method_procces/2_calculate_distance_matrix.py
@@ -8,7 +8,7 @@ Last Update: 2022.08.14
 Describe: 獲取所有時價登入土地與目標土地經緯度之間的直線距離。
 """
 import os
-import itertools
+import tqdm
 import pandas as pd
 
 from geopy.distance import geodesic
@@ -28,7 +28,7 @@ df_tran.drop(['lat', 'long'], axis=1, inplace=True)
 
 # 計算距離
 target_coordinate_col = 'group_center'
-transaction_coordinate_col = 'tran_center'
+transaction_coordinate_col = 'tran_coordinate'
 refer_coordinate_col_ls = ['refer_point1', 'refer_point2', 'refer_point3', 'refer_point4']
 
 target_cols = [target_coordinate_col] + refer_coordinate_col_ls
@@ -39,23 +39,24 @@ def get_distance(x):
         lat1, long1 = x[0].split(',')
         lat2, long2 = x[1].split(',')
 
-        if (abs(lat1) <= 90) & (abs(lat2) <= 90) & (long1<180) & (long2<180):
+        if (abs(float(lat1)) <= 90) & (abs(float(lat2)) <= 90) & (float(long1)<180) & (float(long2)<180):
             return geodesic([lat1, long1],[lat2, long2]).meters
         else:
             return ''
+
     except:
         return ''
 
-for gp_id in df_tar['group_id'].to_list():
+for gp_id in tqdm.tqdm(df_tar['group_id'].to_list()):
     df_coor = df_tran.copy()
     df_coor['group_id'] = gp_id
     df_coor = df_coor.merge(df_tar, how='left')
-    df_coor.to_csv(os.path.join(output_folder, f'group{gp_id}_coordinate.csv'), index=False)
+    # df_coor.to_csv(os.path.join(output_folder, f'group{gp_id}_coordinate.csv'), index=False)
 
     df_coor_dup = df_coor.drop_duplicates([target_coordinate_col, transaction_coordinate_col])
     for col in target_cols:
-        df_coor_dup[f'{col}_DIST'] = list(map(get_distance, df_coor_dup[[transaction_coordinate_col, col]].values)) #  11.9 s/60000 筆 
+        # df_coor_dup[f'{col}_DIST'] = list(map(get_distance, df_coor_dup[[transaction_coordinate_col, col]].values)) #  11.9 s/60000 筆
+        df_coor_dup = df_coor_dup.assign(**{f'{col}_DIST':list(map(get_distance, df_coor_dup[[transaction_coordinate_col, col]].values))})
         df_coor_dup = df_coor_dup[~(df_coor_dup[f'{col}_DIST']=='')]
 
-
-    tmp.to_csv(os.path.join(output_folder, f'group{gp_id}_distance.csv'), index=False)
+    df_coor_dup.to_csv(os.path.join(output_folder, f'group{gp_id}_distance.csv'), index=False)
