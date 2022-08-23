f93cbac239b8b75f69ce99cee17d2dd07c7f4c40
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-23 18:53:39 +0800

    Create 3_target_index.py

---
+++ code/method_procces/3_target_index.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+"""
+Author: 何彥南 (yen-nan ho)
+Github: https://github.com/aaron1aaron2
+Email: aaron1aaron2@gmail.com
+Create Date: 2022.08.23
+Last Update: 2022.08.23
+Describe: 計算每段時間，目標點、參考點在一定範圍內的區域指標
+"""
