baa5e7bce16822502ada8707194bbe7e7bab3263
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-23 18:53:34 +0800

    Update 2_calculate_distance_matrix.py

--- code/method_procces/2_calculate_distance_matrix.py
+++ code/method_procces/2_calculate_distance_matrix.py
@@ -3,8 +3,8 @@
 Author: 何彥南 (yen-nan ho)
 Github: https://github.com/aaron1aaron2
 Email: aaron1aaron2@gmail.com
-Create Date: 2022.08.14
-Last Update: 2022.08.14
+Create Date: 2022.08.16
+Last Update: 2022.08.23
 Describe: 獲取所有時價登入土地與目標土地經緯度之間的直線距離。
 """
 import os
