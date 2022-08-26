04ac64f52491b89bf710314ae327efd6a53209cb
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-26 17:21:17 +0800

    Update data_helper_.py

--- scripts/data_helper_.py
+++ scripts/data_helper_.py
@@ -1,9 +1,11 @@
 # encoding: utf-8
 """
-Author: yen-nan ho
-Contact: aaron1aaron2@gmail.com
-GitHub: https://github.com/aaron1aaron2
-Create Date:  2021.12.19
+Author: 何彥南 (yen-nan ho)
+Github: https://github.com/aaron1aaron2
+Email: aaron1aaron2@gmail.com
+Create Date: 2022.08.26
+Last Update: 2022.08.26
+Describe: 計算每段時間，目標點、參考點在一定範圍內的區域指標
 """
 import os
 import argparse
