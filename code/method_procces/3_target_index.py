c517e37ebba4dc83370eaf3e6f58c30941b374b1
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-26 17:21:25 +0800

    Update 3_target_index.py

--- code/method_procces/3_target_index.py
+++ code/method_procces/3_target_index.py
@@ -17,10 +17,10 @@ output_folder = 'data/method_procces/3_target_index'
 os.makedirs(output_folder, exist_ok=True)
 
 dist_threshold = 3000
+target_value_col = '單價元平方公尺'
 
 start_date = '2012-7'
 end_date = '2022-4'
-
 
 target_coordinate_col = 'group_center'
 transaction_coordinate_col = 'tran_coordinate'
@@ -32,7 +32,12 @@ target_cols = [i + '_DIST' for i in target_cols]
 df_tran = pd.read_csv('data/data_procces/8_time_range_select/transaction_all.csv', usecols=['land_id', 'year', 'month', 'day', '單價元平方公尺'])
 df_tran = df_tran.astype(int)
 
-date_table = []
+# 日期基準
+date_ls = pd.date_range(start=start_date, end=end_date, freq='M')
+date_table = pd.DataFrame({
+        'year': date_ls.year,
+        'month': date_ls.month
+    })
 
 for gp_file in os.listdir(distance_matrix_folder):
     df_distance = pd.read_csv(os.path.join(distance_matrix_folder, gp_file), usecols=['land_id'] + target_cols)
@@ -40,10 +45,27 @@ for gp_file in os.listdir(distance_matrix_folder):
         id_select = df_distance.loc[df_distance[col] <= dist_threshold, 'land_id'].to_list()
         df_tran_select = df_tran[df_tran['land_id'].isin(id_select)].copy()
 
+        # 計算區域指標 >>>>>>>>>>>>>>>>>>>
         # 區域內每月交易數量
         # df_tran_select.groupby(['year', 'month'])['單價元平方公尺'].count()
 
         # 區域內每月平均價
-        month_mean = df_tran_select.groupby(['year', 'month'])['單價元平方公尺'].mean()
+        month_mean = df_tran_select.groupby(['year', 'month'])[target_value_col].mean()
         # month_mean.plot()
         # plt.show()
+        # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        # 並到時間表，缺直處理 >>>>>>>>>>>>
+        month_mean = date_table.merge(month_mean.reset_index(), how='left')
+        # month_mean = date_table.merge(month_mean.reset_index().drop(115).drop(113), how='left') # 測試用
+        na_num = month_mean[month_mean[target_value_col].isna()].shape[0]
+
+        f_fill = month_mean[target_value_col].fillna(method='ffill')
+        b_fill = month_mean[target_value_col].fillna(method='bfill')
+
+        f_b_avg_fill  = (f_fill + b_fill)/2
+
+        unable_fill_na_num = f_b_avg_fill[f_b_avg_fill.isna()].shape[0]
+        
+        f_b_avg_fill.dropna(inplace=True)
+        # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
