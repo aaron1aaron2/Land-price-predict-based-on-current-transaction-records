28ae925232954106c97d08af0f04158592d95dfd
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-08-31 18:03:55 +0800

    Update reference_point.py

--- method/reference_point.py
+++ method/reference_point.py
@@ -1,2 +1,11 @@
+# encoding: utf-8
+"""
+Author: 何彥南 (yen-nan ho)
+Github: https://github.com/aaron1aaron2
+Email: aaron1aaron2@gmail.com
+Create Date: 2022.08.31
+Last Update: 2022.08.31
+Describe: 建立參考點的 function，可以放在這。
+"""
 def UDLR():
     print('test')
