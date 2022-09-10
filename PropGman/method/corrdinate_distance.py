8d463ace044e779565ba60f3899a21b1c430b3ee
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-09-10 15:03:21 +0800

    Update corrdinate_distance.py

--- PropGman/method/corrdinate_distance.py
+++ PropGman/method/corrdinate_distance.py
@@ -7,13 +7,10 @@ Create Date: 2022.09.09
 Last Update: 2022.09.09
 Describe: 獲取所有時價登入土地與目標土地經緯度之間的直線距離。
 """
-import tqdm
-import pandas as pd
-
 from geopy.distance import geodesic
 
 def get_distance(x):
-    """部分經緯度有問題"""
+    """部分經緯度格式有問題"""
     try:
         lat1, long1 = x[0].split(',')
         lat2, long2 = x[1].split(',')
