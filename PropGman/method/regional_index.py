b1e1e022c8cd16e3c6ea5b97febf4d97b3b55abd
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-09-23 17:47:37 +0800

    Update regional_index.py

--- PropGman/method/regional_index.py
+++ PropGman/method/regional_index.py
@@ -8,6 +8,7 @@ Last Update: 2022.09.13
 Describe: 計算區域性指標
 """
 import pandas as pd
+from typing import Tuple
 
 class RegionalIndex:
     def __init__(self, start_date:str, end_date:str, time_freq:str, dist_threshold:int):
@@ -35,15 +36,21 @@ class RegionalIndex:
 
         return date_table
 
-    def _fill_na(self, df, col):
+    def _fill_na(self, df, col, method):
         na_num = df[df[col].isna()].shape[0]
 
-        f_fill = df[col].fillna(method='ffill')
-        b_fill = df[col].fillna(method='bfill')
+        if method=='front-back-avg':
+            f_fill = df[col].fillna(method='ffill')
+            b_fill = df[col].fillna(method='bfill')
 
-        f_b_avg_fill  = (f_fill + b_fill)/2
+            f_b_avg_fill  = (f_fill + b_fill)/2
 
-        unable_fill_na_num = f_b_avg_fill[f_b_avg_fill.isna()].shape[0]
+            unable_fill_na_num = f_b_avg_fill[f_b_avg_fill.isna()].shape[0]
+
+        elif method=='zero':
+            df[col] = df[col].fillna(0)
+
+            unable_fill_na_num = 0
 
         record = {
             'column_name':col, 
@@ -54,25 +61,33 @@ class RegionalIndex:
 
         return f_b_avg_fill, record
 
-    def get_index(self, df_distance:pd.DataFrame, df_tran:pd.DataFrame, method:str, target_value_col:str, dist_value_col:str, id_col:str):
+    def get_index(
+            self, 
+            df_distance:pd.DataFrame, 
+            df_tran:pd.DataFrame, 
+            method:str, 
+            target_value_col:str, 
+            dist_value_col:str, 
+            id_col:str, 
+            fillna_method:str
+        ) -> Tuple[pd.DataFrame, dict]:
+
         id_select = df_distance.loc[df_distance[dist_value_col] <= self.dist_threshold, id_col].to_list()
         df_tran_select = df_tran[df_tran[id_col].astype(int).isin(id_select)].copy()
 
         df_tran_select[target_value_col] = df_tran_select[target_value_col].astype(float)
         df_tran_select['year'] = df_tran_select['year'].astype(int)
         df_tran_select['month'] = df_tran_select['month'].astype(int)
-        from IPython import embed
-        embed()
-        exit()
+
         if method=='mean':
-            month_mean = df_tran_select.groupby(['year', 'month'])[target_value_col].mean()
+            gb_result = df_tran_select.groupby(['year', 'month'])[target_value_col].mean()
         elif method=='count':
-            month_mean = df_tran_select.groupby(['year', 'month'])[target_value_col].count()
+            gb_result = df_tran_select.groupby(['year', 'month'])[target_value_col].count()
         else:
-            month_mean = df_tran_select.groupby(['year', 'month'])[target_value_col].mean()
+            gb_result = df_tran_select.groupby(['year', 'month'])[target_value_col].mean()
 
-        month_mean = self.date_table.merge(month_mean.reset_index(), how='left')
+        gb_result = self.date_table.merge(gb_result.reset_index(), how='left')
 
-        result, record = self._fill_na(month_mean, target_value_col)
+        result, record = self._fill_na(gb_result, target_value_col, fillna_method)
 
         return result, record
