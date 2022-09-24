ad52c53ee34083ed56d1a461d9180a082407cad2
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-09-24 19:08:18 +0800

    Update train_basic.sh

--- scripts/train_basic.sh
+++ scripts/train_basic.sh
@@ -1,7 +1,7 @@
 train_data="./data/train_data/basic/train_data/mean_group0_dist3000.h5"
 SEfile="./data/train_data/basic/SE_data/group0/SE.txt"
 output_folder_root="./output/basic/group0"
-python train.py --num_his 5 --num_pred 1 --batch_size 24 \
+python train.py --time_slot 1440 --num_his 5 --num_pred 1 --batch_size 24 \
         --max_epoch 100 --patience 100 --learning_rate 0.01 \
         --traffic_file "$train_data" \
         --SE_file "$SEfile" \
