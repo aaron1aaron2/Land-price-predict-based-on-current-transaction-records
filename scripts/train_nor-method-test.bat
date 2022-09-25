d2b09dbabf3bc5935b34ea442be991813059b2ce
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-09-25 16:48:08 +0800

    Update train_nor-method-test.bat

--- scripts/train_nor-method-test.bat
+++ scripts/train_nor-method-test.bat
@@ -1,3 +1,6 @@
+@REM 關掉回傳命令，不會回傳 bat 程式碼本身
+@echo off 
+
 set train_data="./data/train_data/basic/train_data/mean_group0_dist3000.h5"
 set SEfile="./data/train_data/basic/SE_data/group0/SE.txt"
 
@@ -10,26 +13,26 @@ python train.py --time_slot 1440 --num_his 5 --num_pred 1 --batch_size 24 ^
         --model_file %output_folder_root%/model.pkl ^
         --log_file %output_folder_root%/log.txt ^
         --output_folder %output_folder_root% ^
-        --device gpu
+        --device cpu 
 
-@REM set output_folder_root="./output/nor-method-test/z-score"
-@REM python train.py --time_slot 1440 --num_his 5 --num_pred 1 --batch_size 24 ^
-@REM         --max_epoch 100 --patience 100 --learning_rate 0.01 ^
-@REM         --normalization_method z-score ^
-@REM         --traffic_file %train_data% ^
-@REM         --SE_file %SEfile% ^
-@REM         --model_file %output_folder_root%/model.pkl ^
-@REM         --log_file %output_folder_root%/log.txt ^
-@REM         --output_folder %output_folder_root% ^
-@REM         --device gpu
+set output_folder_root="./output/nor-method-test/z-score"
+python train.py --time_slot 1440 --num_his 5 --num_pred 1 --batch_size 24 ^
+        --max_epoch 100 --patience 100 --learning_rate 0.01 ^
+        --normalization_method z-score ^
+        --traffic_file %train_data% ^
+        --SE_file %SEfile% ^
+        --model_file %output_folder_root%/model.pkl ^
+        --log_file %output_folder_root%/log.txt ^
+        --output_folder %output_folder_root% ^
+        --device cpu
 
-@REM set output_folder_root="./output/nor-method-test/log"
-@REM python train.py --time_slot 1440 --num_his 5 --num_pred 1 --batch_size 24 ^
-@REM         --max_epoch 100 --patience 100 --learning_rate 0.01 ^
-@REM         --normalization_method log ^
-@REM         --traffic_file %train_data% ^
-@REM         --SE_file %SEfile% ^
-@REM         --model_file %output_folder_root%/model.pkl ^
-@REM         --log_file %output_folder_root%/log.txt ^
-@REM         --output_folder %output_folder_root% ^
-@REM         --device gpu
+set output_folder_root="./output/nor-method-test/log"
+python train.py --time_slot 1440 --num_his 5 --num_pred 1 --batch_size 24 ^
+        --max_epoch 100 --patience 100 --learning_rate 0.01 ^
+        --normalization_method log ^
+        --traffic_file %train_data% ^
+        --SE_file %SEfile% ^
+        --model_file %output_folder_root%/model.pkl ^
+        --log_file %output_folder_root%/log.txt ^
+        --output_folder %output_folder_root% ^
+        --device cpu
