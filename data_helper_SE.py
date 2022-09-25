40fe152558a7ed3f2e344d5492d6fd83dee0a1d5
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-09-25 16:11:24 +0800

    Update data_helper_SE.py

--- data_helper_SE.py
+++ data_helper_SE.py
@@ -3,149 +3,16 @@
 Author: yen-nan ho
 Contact: aaron1aaron2@gmail.com
 GitHub: https://github.com/aaron1aaron2
-Create Date:  2021.12.13
+Create Date:  2022.09.25
 """
 import os
 import argparse
-import itertools
 import pandas as pd
-import numpy as np
-from geopy.distance import geodesic
 
-from src.utils import build_folder, saveJson, str2bool
-from model.node2vec import generateSE 
+from PropGman.utils import *
+from PropGman.spatial_embedding import *
 
 pd.options.mode.chained_assignment = None  # default='warn'
-
-def get_one_way_edge(df, group:str, coor_col:str, id_col:str):
-    '''
-    Group according to G, create a one-way-edge for all points in each group.
-
-    point out
-    ---
-    There will be `(n-1)*n/2` lines(edge) at n points
-    '''
-    # 檢查輸入參數
-    if group != None:
-        cols = [group, coor_col, id_col]
-    else:
-        cols = [coor_col, id_col]
-
-    for col in cols:
-        assert col in df.columns, "'{}' not in dataframe! please check a column name.".format(col)
-    
-    df = df[cols]
-
-    #產生配對
-    if group != None:
-        df_AB = df.groupby(df[group]).apply(lambda x:pd.DataFrame(list(itertools.combinations(x[id_col], 2))))
-    else:
-        df_AB = pd.DataFrame(list(itertools.combinations(df[id_col], 2)))
-
-    df_AB.rename(columns={
-                    0:'start_id',
-                    1:'end_id'
-                   }, 
-                 inplace=True)
-
-    #資料合併
-    df.rename(columns={
-                 '{}'.format(coor_col):'start_latlong',
-                 '{}'.format(id_col):'start_id'
-                }, 
-              inplace=True)
-
-    df_AB = pd.merge(df_AB, df, on=['start_id'], how='left')
-
-    df.rename(columns={
-                'start_latlong':'end_latlong',
-                'start_id':'end_id'
-                }, 
-              inplace=True)
-
-    df_AB = pd.merge(df_AB, df, on=['end_id'], how='left')
-    
-    return df_AB
-
-def get_two_way_with_self(df, one_wey_table, coor_col:str, id_col:str):
-    '''
-    Flip two points and add the connection between the same points.
-    Then we can get table, which can convert to  `distance matrix`.
-
-    warn
-    ---
-    `start_id`, `end_id`, `start_latlong`, `end_latlong`, `linear_distance` must exist in the field.
-    '''
-
-    # 檢查參數
-    for arg in [coor_col, id_col]:
-        assert arg in df.columns, "[{}] not in dataframe input".format(arg)
-
-    df = df[[id_col, coor_col]]
-
-    #翻轉AB
-    df.rename(columns={
-        '{}'.format(id_col):'start_id',
-        '{}'.format(coor_col):'start_latlong'}, inplace=True)
-
-
-    df_end = df[['start_id','start_latlong']].rename(columns={
-                                                        'start_id':'end_id',
-                                                        'start_latlong':'end_latlong'
-                                                        })
-    df_self = pd.concat([df,df_end],sort=True,axis=1)
-    
-    #自己到自己補零
-    df_self['linear_distance'] = np.zeros(shape=(len(df),1))
-
-    one_wey_table_T = one_wey_table.rename(columns={
-                                                'start_latlong':'end_latlong',
-                                                'end_latlong':'start_latlong',
-                                                'start_id':'end_id',
-                                                'end_id':'start_id',
-                                                'start_addr':'end_addr',
-                                                'end_addr':'start_addr'
-                                                })
-
-    two_wey_table = pd.concat([one_wey_table, one_wey_table_T, df_self], sort=True)
-
-    no = two_wey_table['start_id'].astype(str).str.extract(r'(?P<start_no>\d+)').astype(int)
-    no2 = two_wey_table['end_id'].astype(str).str.extract(r'(?P<end_no>\d+)').astype(int)
-
-    two_wey_table = pd.concat([two_wey_table, no, no2], axis=1)
-
-    two_wey_table.sort_values(['start_no','end_no'], inplace=True) 
-
-
-    return two_wey_table[['start_no', 'end_no', 'start_id', 'end_id', 'start_latlong', 'end_latlong', 'linear_distance']]
-
-def get_linear_distance(df):
-    '''
-    Use two points of latitude and longitude to get the straight line distance.
-
-    warn
-    ---
-    `start_latlong` and `end_latlong` must exist in the field. 
-
-    Both `start_latlong` and `end_latlong` need to comply with the following format: "25.10514,121.5182"
-    '''
-    for col in ['start_latlong', 'end_latlong']:
-        assert (col in df.columns)
-
-    df['linear_distance'] = df.apply(lambda x:geodesic(x['start_latlong'].split(','),x['end_latlong'].split(',')).meters,axis=1)
-
-    return df
-
-def get_adj_value(df, threshold=0):
-    '''The formula is `exponent (-1 * square (df['linear_distance']) / square (variance))`'''
-    assert threshold>=0, '`threshold` must be greater than zero'
-    Variation = np.var(df['linear_distance'].values)
-    adj_ls = np.exp(-1*np.square(df['linear_distance'].values)/Variation)
-    df['adj'] = list(map(lambda x: x if x >= threshold else 0, adj_ls))
-
-    return df
-
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -156,7 +23,6 @@ def get_args():
     parser.add_argument('--id_col', type=str, default='sno', help='點的編號欄位')
     parser.add_argument('--group_col', type=str, default='sarea', help='群組欄位(youbike 資料以區為單位分區域)')
     parser.add_argument('--group', type=str, default=None, help='使用的群組(需要指定 group_col)，格式: XX區,XX區')
-    parser.add_argument('--use_group', type=str2bool, default=False, help='是否要每個 group 分別去建立點之間的連結')
 
     parser.add_argument('--coordinate_col', type=str, default=None, help='點的經緯度欄位(當需要計算距離時)，格式: 24.1580,121.6222')
     parser.add_argument('--longitude_col', type=str, default='lng', help='經度(當 --coordinate_col 未設定時會使用)')
@@ -181,8 +47,6 @@ def get_args():
     args = parser.parse_args()
 
     return args 
-
-
 
 if __name__ == "__main__":
 
@@ -230,10 +94,7 @@ if __name__ == "__main__":
         # 建立區域內連結
         print("number of nodes: {}".format(df.shape[0]))
 
-        if args.use_group: 
-            df_AB = get_one_way_edge(df, group=args.group_col, coor_col=args.coordinate_col, id_col=args.id_col)
-        else:
-            df_AB = get_one_way_edge(df, group=None, coor_col=args.coordinate_col, id_col=args.id_col)
+        df_AB = get_one_way_edge(df, group=group_col, coor_col=coordinate_col, id_col=id_col)
 
         # 獲取各 edge 關係評估值
         print("shape of one way edge: {}".format(df_AB.shape))
@@ -242,15 +103,11 @@ if __name__ == "__main__":
         else:
             assert False, 'please set the parameter - `distance_method`'
 
-        df_AB.to_csv(os.path.join(args.output_folder, 'one_way_edge_table_LD.csv'), index=False)
-
         # 建立雙向 edge 和自己到自己 (可直接轉成 disatnce martix)
         df_2W = get_two_way_with_self(df, df_AB, coor_col=args.coordinate_col, id_col=args.id_col)
-        df_2W.to_csv(os.path.join(args.output_folder, 'two_way_edge_table_LD.csv'), index=False)
 
         # 計算 adj 值 (基於 GMAN 論文上的算法，越小關係越大)
         df_2W_adj = get_adj_value(df_2W, threshold=args.adj_threshold)
-        df_2W_adj.to_csv(os.path.join(args.output_folder, 'two_way_edge_table_LD(adj).csv'), index=False)
 
         df_2W_adj[['start_no', 'end_no', 'adj']].to_csv(Adj_file, sep=' ', index=False, header=False)
 
