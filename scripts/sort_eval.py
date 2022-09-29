f1ebe19e4c6cad0b5c5b7dfd591bce38e5824734
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-09-29 22:37:45 +0800

    Update sort_eval.py

--- scripts/sort_eval.py
+++ scripts/sort_eval.py
@@ -14,7 +14,7 @@ import pandas as pd
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--target_folder', type=str, default='output/test-hyper-parameter(amount)')
+    parser.add_argument('--t', type=str, default='output/test-hyper-parameter(amount)')
     parser.add_argument('--output', type=str, default=None)
 
     args = parser.parse_args()
@@ -24,14 +24,14 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
 
-    eval_ls = glob.glob(os.path.join(args.target_folder, '**', '*evaluation.json'), recursive=True)
+    eval_ls = glob.glob(os.path.join(args.t, '**', '*evaluation.json'), recursive=True)
     eval_result_ls = [json.load(open(i)) for i in eval_ls]
 
     result = pd.DataFrame(eval_result_ls)
 
     result = pd.concat([result, pd.DataFrame(eval_ls, columns=['file'])], axis=1)
 
-    tmp = result['file'].str.replace(args.target_folder, '', regex=False)
+    tmp = result['file'].str.replace(args.t, '', regex=False)
     tmp = tmp.str.extract('(?P<tmp>.+).evaluation.json')['tmp'].str.strip('\\').str.split('\\', expand=True)
 
     tmp.columns = [f'layer{i+1}' for i in tmp.columns]
@@ -41,4 +41,4 @@ if __name__ == '__main__':
     if args.output:
         result.to_csv(args.output, index=False)
     else:
-        result.to_csv(os.path.join(args.target_folder, 'result.csv'), index=False)
+        result.to_csv(os.path.join(args.t, 'result.csv'), index=False)
