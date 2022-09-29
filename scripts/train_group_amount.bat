621ecb9c257d2a040a033f32d1e38fd2fddebe27
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-09-29 22:37:51 +0800

    Update train_group_amount.bat

--- scripts/train_group_amount.bat
+++ scripts/train_group_amount.bat
@@ -7,7 +7,7 @@ set output_folder_root="./output/test-group-amount"
 for /L %%X in (0,1,%gp_num%) do (
     echo "group%%X"
 
-    python train.py --time_slot 1440 --num_his 3 --num_pred 1 --batch_size 16 ^
+    python train.py --time_slot 1440 --num_his 3 --num_pred 1 --batch_size 8 ^
                     --max_epoch 50 --patience 100 --learning_rate 0.001 ^
                     --traffic_file "./data/train_data/transaction_amount/train_data/count_group%%X_dist3000.h5" ^
                     --SE_file "./data/train_data/transaction_amount/SE_data/group%%X/SE.txt" ^
