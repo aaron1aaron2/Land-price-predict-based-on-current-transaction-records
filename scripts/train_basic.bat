dfe8decc9a011319ff1b98645ce7221609c96987
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-09-29 22:37:49 +0800

    Update train_basic.bat

--- scripts/train_basic.bat
+++ scripts/train_basic.bat
@@ -1,8 +1,8 @@
 set train_data="./data/train_data/basic/train_data/mean_group0_dist3000.h5"
 set SEfile="./data/train_data/basic/SE_data/group0/SE.txt"
-set output_folder_root="./output/basic_10/"
-python train.py --time_slot 1440 --num_his 3 --num_pred 1 --batch_size 24 ^
-        --max_epoch 50 --patience 100 --learning_rate 0.01 ^
+set output_folder_root="./output/basic/"
+python train.py --time_slot 1440 --num_his 3 --num_pred 1 --batch_size 16 ^
+        --max_epoch 100 --patience 100 --learning_rate 0.0001 ^
         --traffic_file %train_data% ^
         --SE_file %SEfile% ^
         --model_file %output_folder_root%/model.pkl ^
