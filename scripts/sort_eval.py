45a1dfd65aadf2fc0c00c2d2e42f48925e1997ff
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-09-29 14:11:04 +0800

    Update sort_eval.py

--- scripts/sort_eval.py
+++ scripts/sort_eval.py
@@ -3,19 +3,19 @@
 Author: yen-nan ho
 Contact: aaron1aaron2@gmail.com
 GitHub: https://github.com/aaron1aaron2
-Create Date: 2022.09.26
-Last Update: 2022.09.26
+Create Date: 2022.09.29
+Last Update: 2022.09.29
 """
 import os
+import json
 import glob
 import argparse
 import pandas as pd
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output_folder', type=str, default='output/test-hyper-parameter(amount)',
-                        help='a time step is mins')
-
+    parser.add_argument('--target_folder', type=str, default='output/test-hyper-parameter(amount)')
+    parser.add_argument('--output', type=str, default=None)
 
     args = parser.parse_args()
 
@@ -23,8 +23,22 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
-    from IPython import embed
-    embed()
-    exit()
-    eval_ls = glob.glob(os.path.join(args.output_folder, '**', '*evaluation.json'), recursive=True)
-    eval_ls
+
+    eval_ls = glob.glob(os.path.join(args.target_folder, '**', '*evaluation.json'), recursive=True)
+    eval_result_ls = [json.load(open(i)) for i in eval_ls]
+
+    result = pd.DataFrame(eval_result_ls)
+
+    result = pd.concat([result, pd.DataFrame(eval_ls, columns=['file'])], axis=1)
+
+    tmp = result['file'].str.replace(args.target_folder, '', regex=False)
+    tmp = tmp.str.extract('(?P<tmp>.+).evaluation.json')['tmp'].str.strip('\\').str.split('\\', expand=True)
+
+    tmp.columns = [f'layer{i+1}' for i in tmp.columns]
+
+    result = tmp.join(result)
+
+    if args.output:
+        result.to_csv(args.output, index=False)
+    else:
+        result.to_csv(os.path.join(args.target_folder, 'result.csv'), index=False)
